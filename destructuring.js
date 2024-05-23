71ddfd897d6d68b82f8b92086fc87bb5bf78ee7c
NickBwalley <nickbwalley@gmail.com>
2024-05-23 12:24:19 +0300

    array destructuring

---
+++ destructuring.js
@@ -0,0 +1,7 @@
+// Destructuring: easily extract array elements or object properties and store them in variables
+// ARRAY DESTRUCTURING
+const numbers = [1, 2, 3];
+[num1, num2] = numbers;
+console.log(num1, num2);
+
+// OBJECT DESTRUCTURING.
